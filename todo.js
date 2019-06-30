69564181fae7933512d7f6efc344880b16b0f920
Uh628754 <uh628754@gmail.com>
2019-06-30 12:53:11 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,51 @@
+
+var ul = document.getElementById('list');
+var li;
+
+var addButton = document.getElementById('add');
+addButton.addEventListener('click', addItem);
+
+var removeButton = document.getElementById('remove');
+removeButton.addEventListener('click', removeItem);
+
+function addItem() {
+    var input = document.getElementById("input");
+    var item = input.value;
+    var textnode = document.createTextNode(item);
+    if(item === '') {
+        return false;
+        //var err = document.createElement('p').appendChild("Enter your todo");
+        //document.querySelector('#container').appendChild(err);
+    } else {
+        li = document.createElement('li');
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.setAttribute('id', 'check');
+        
+        var label = document.createElement('label');
+        label.setAttribute('for', 'item');
+
+        //add this element on webpage
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        label.appendChild(textnode);
+        li.appendChild(label);
+        ul.insertBefore(li, ul.childNodes[0]);
+
+        setTimeout(() => {
+            li.className = 'visual';
+        }, 150);
+        
+        input.value = '';
+    }
+    
+}
+
+function removeItem() {
+    li = ul.children;
+    for (let index = 0; index < li.length; index++) {
+        while ( li[index] && li[index].children[0].checked) {
+            ul.removeChild(li[index]);
+        }
+    }
+}
